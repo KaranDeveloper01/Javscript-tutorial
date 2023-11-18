456cf90217afe4b4623813f563597399b0478547
Karankalyana <149147429+KaranDeveloper01@users.noreply.github.com>
2023-11-18 17:22:31 +0000

    Conversions in js

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,34 @@
+// Conversions in javascript
+
+// let score = 55;
+// let score = "55abc";
+// let score = null;
+// let score = undefined;
+// let score = true;
+
+
+// console.log(typeof score);
+
+// Conversion Method
+// let alwaysNumber = Number(score);
+// console.log(typeof score)
+// console.log(alwaysNumber)
+
+// 55 => number
+// "55abc" => NAN :- not a number
+// null => object
+// undefined => undefined
+// true => 1, false => 0
+
+let value = "";
+let valueAssigned = Boolean(value);
+
+console.log(typeof value)
+console.log(valueAssigned)
+
+
+// 1 => True , 0 => false
+// "" => false 
+// "Karan" => true
+
+// Same can be done with other datatypes
